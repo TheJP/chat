303b2b4952ddd118e6a14c59bf76be0271c0d228
Janis Peyer <jp_55555@hotmail.com>
2014-10-12 16:18:29 +0200

    Small update on the protocol

--- server/qtserver/protocol.h
+++ server/qtserver/protocol.h
@@ -16,10 +16,17 @@ enum RquestType {
     OpenConversation = 32,  //Gets latest messages and registers session to conversation -> Called when opening conversation
     CloseConversation = 33, //Unregister session from conversation
     GetMessagesOlder = 34,  //Messages before a given timestamp -> Called to get older messages then the given
+    CreateConversation = 35,
+    DeleteConversation = 36,
+    ChangeConversation = 37,
+    InviteConversation = 38,
+    ChangeConversationPermission = 39,
+    GetConversations = 40, //Public or joined private Conversations as argument
 
     //Admin Operations
     Mute = 51, //Temp (with timestamp) or Perma as parameter
-    Ban = 53,  //Temp (with timestamp) or Perma as parameter
+    Ban = 52,  //Temp (with timestamp) or Perma as parameter
+    ChangePermission = 53
 
 };
 enum ResponseType {
